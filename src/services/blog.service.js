49febb06282cd233ed8a7d6410dc57f755f9bf99
estol-web-design <estolwebdesign@outlook.com>
2024-01-04 19:12:37 -0300

    Create blog.service.js

---
+++ src/services/blog.service.js
@@ -0,0 +1,281 @@
+import DOMPurify from "dompurify";
+import DefaultPost from "../models/post.model.js";
+import { validateNewPostData, validateUpdatePostData } from "../utils/validatePosts.js";
+
+export const getAllPosts = async ({ model = DefaultPost, fieldsToPopulate = [], useLean = true }) => {
+   try {
+      // Initializing search query
+      let query = model.find();
+
+      // Check if the user needs to populate any fields and perform population
+      if (fieldsToPopulate.length > 0) {
+         query.populate(fieldsToPopulate.join(" "));
+      }
+
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const posts = !useLean ? await query : await query.lean();
+
+      if (posts.length < 1) {
+         return { success: false, message: "No post found", code: 404 };
+      }
+
+      // Return the found posts to the user
+      return { success: true, posts, code: 200 };
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const getLastPosts = async ({ model = DefaultPost, fieldsToPopulate = [], originalQuantity = null, flag = null, useLean = true }) => {
+   // Check if original quantity parameter is valid. If it's not valid assign default value
+   const quantity = typeof originalQuantity === "number" && originalQuantity > 0 ? originalQuantity : 10;
+
+   try {
+      // Create DateFlag for search query
+      const dateFlag = flag ? new Date(flag) : new Date();
+
+      // Create an object with search parameters
+      const searchParams = { createdAt: { $lt: dateFlag } };
+
+      // Initialize search query with sort and limit methods
+      let query = model.find(searchParams).sort("-createdAt").limit(quantity);
+
+      // Check if the user needs to populate any fields and perform population
+      if (fieldsToPopulate.length > 0) {
+         query = query.populate(fieldsToPopulate.join(" "));
+      }
+
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const posts = !useLean ? await query : await query.lean();
+
+      // Send an error message if no posts were found
+      if (posts.length < 1) {
+         return { success: false, message: "No post found", code: 404 };
+      }
+
+      const newFlag = posts[posts.length-1].createdAt;
+
+      // Object to return
+      const returnObj = { success: true, posts, code: 200, newFlag };
+
+      // Check if quantity parameter provided by the user is an invalid number, and add a message to the object to return
+      if (typeof originalQuantity === "number" && originalQuantity < 1) {
+         returnObj.message = `Invalid value of ${originalQty} assigned to quantity parameter, returning 10 posts by default.`;
+      }
+
+      return returnObj;
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const getTrendingPosts = async ({ model = DefaultPost, fieldsToPopulate = [], originalQuantity = null, useLean = true }) => {
+   // Check if original quantity parameter is valid. If it's not valid assign default value
+   const quantity = typeof originalQuantity === "number" && originalQuantity > 0 ? originalQuantity : 10;
+
+   try {
+      // Initialize search query with sort and limit methods
+      let query = model.find().sort("-views").limit(quantity);
+
+      // Check if the user needs to populate any fields and perform population
+      if (fieldsToPopulate.length > 0) {
+         query = query.populate(fieldsToPopulate.join(" "));
+      }
+
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const posts = !useLean ? await query : await query.lean();
+
+      // Send an error message if no posts were found
+      if (posts.length < 1) {
+         return { success: false, message: "No post found", code: 400 };
+      }
+
+      // Object to return
+      const returnObj = { success: true, posts, code: 200 };
+
+      // Check if quantity parameter provided by the user is an invalid number, and add a message to the object to return
+      if (typeof originalQty === "number" && originalQty < 1) {
+         returnObj.message = `Invalid value of ${originalQty} assigned to quantity parameter, returning 10 posts by default.`;
+      }
+
+      return returnObj;
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const searchPosts = async ({ model = DefaultPost, fieldsToPopulate = [], originalQuantity = null, useLean = true, flag = null, searchStr = null }) => {
+   // Check if original quantity parameter is valid. If it's not valid assign default value
+   const quantity = typeof originalQuantity === "number" && originalQuantity > 0 ? originalQuantity : 10;
+
+   try {
+      // Check if search string was provided
+      if (!searchStr) {
+         return { success: false, message: `No search string ("searchStr" parameter) provided to perform posts search`, code: 400 };
+      }
+
+      // Create DateFlag for search query
+      const dateFlag = flag ? new Date(flag) : new Date();
+
+      // Create an object with search parameters
+      const searchParams = { createdAt: { $lt: dateFlag }, $or: [{ title: { $regex: searchStr, options: "i" } }, { content: searchStr, options: "i" }] };
+
+      // Initialize search query with sort and limit methods
+      let query = model.find(searchParams).sort("-createdAt").limit(quantity);
+
+      // Check if the user needs to populate any fields and perform population
+      if (fieldsToPopulate.length > 0) {
+         query = query.populate(fieldsToPopulate.join(" "));
+      }
+
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const posts = !useLean ? await query : await query.lean();
+
+      if (posts.length < 1) {
+         return { success: false, message: "No post found", code: 404 };
+      }
+
+      // Object to return
+      const returnObj = { success: true, posts, code: 200 };
+
+      // Check if quantity parameter provided by the user is an invalid number, and add a message to the object to return
+      if (typeof originalQuantity === "number" && originalQuantity < 1) {
+         returnObj.message = `Invalid value of "${originalQuantity}" assigned to quantity parameter, returning 10 posts by default.`;
+      }
+
+      return returnObj;
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const getOnePost = async ({ model = DefaultPost, fieldsToPopulate = [], useLean = true, id = null }) => {
+   try {
+      // Check if an id was provided to perform post search
+      if (!id) {
+         return { success: false, message: "No id provided to perform post search", code: 400 };
+      }
+
+      // Initialize post search query
+      let query = model.findById(id);
+
+      // Check if the user needs to populate any fields and perform population
+      if (fieldsToPopulate.length > 0) {
+         query = query.populate(fieldsToPopulate.join(" "));
+      }
+
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const post = !useLean ? await query : await query.lean();
+
+      // Check if post was successfully find
+      if (!post) {
+         return { success: false, message: "No post found", code: 404 };
+      }
+
+      return { success: true, post, code: 200 };
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const createPost = async ({ model = DefaultPost, useLean = true, newPost = null }) => {
+   try {
+      // Check if new post parameters were provided, and if those parameters are valid
+      if (!newPost) {
+         return { success: false, message: `No post data ("newPost" parameter) provided to create the new post`, code: 400 };
+      } else if (model === DefaultPost) {
+         const validPost = validateNewPostData(newPost);
+
+         if (!validPost.success) {
+            return { success: false, message: validPost.message, code: 400 };
+         }
+      }
+
+      // Sinitize HTML post's content
+      const sanitizedHTML = DOMPurify.sanitize(newPost.content);
+      newPost.content = sanitizedHTML;
+
+      // Create post with validated parameters
+      const createdPost = await model.create(newPost);
+
+      // Verify if post was successfully created
+      if (!createdPost) {
+         return { success: false, message: "Failed to create new post.", code: 500 };
+      }
+
+      return { success: true, code: 201 };
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const updatePost = async ({ model = DefaultPost, fieldsToPopulate = [], useLean = true, id = null, updatedData = null }) => {
+   try {
+      // Check if an id was provided to perform post search
+      if (!id) {
+         return { success: false, message: "No post ID provided. Please provide a valid post ID to perform the update.", code: 400 };
+      }
+
+      // Check if update post parameters ("updateData") were provided, and if those parameters are valid
+      if (!updatedData) {
+         return { success: false, message: `No updeted post data ("updatedData" parameter) provided to update this post`, code: 400 };
+      } else if (model === DefaultPost) {
+         const validData = validateUpdatePostData(updatedData);
+
+         if (!validData.success) {
+            return { success: false, message: validData.message, code: 400 };
+         }
+      }
+
+      // Sanitize HTML post's content
+      let sanitizedHTML;
+      if (updatedData.content) {
+         sanitizedHTML = DOMPurify.sanitize(updatedData.content);
+         updatedData.content = sanitizedHTML;
+      }
+
+      // Initialize post update query
+      let query = model.findByIdAndUpdate(id, updatedData);
+
+      // Check if the user needs to populate any fields and perform population
+      if (fieldsToPopulate.length > 0) {
+         query = query.populate(fieldsToPopulate.join(" "));
+      }
+
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const oldPost = !useLean ? await query : await query.lean();
+
+      // Check if post was successfully updated
+      if (!oldPost) {
+         return { success: false, message: "Failed to update post", code: 500 };
+      }
+
+      return { success: true, oldPost, code: 200 };
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
+
+export const deletePost = async ({ model = DefaultPost, useLean = true, id = null }) => {
+   try {
+      // Check if an id was provided to perform post search
+      if (!id) {
+         return { success: false, message: "No post ID provided. Please provide a valid post ID to perform post deletion.", code: 400 };
+      }
+
+      // Delete post
+      let query = model.findByIdAndDelete(id);
+      
+      // Check if the user needs a complete instance of the model; otherwise, send a plain JavaScript object
+      const deletedPost = !useLean ? await query : await query.lean();
+
+      // Check if post was successfully deleted
+      if (!deletedPost) {
+         return { success: false, message: "Failed to delete this post", code: 500 };
+      }
+
+      return { success: true, deletedPost, code: 200 };
+   } catch (err) {
+      return { success: false, message: err.message, code: 500, error: err };
+   }
+};
